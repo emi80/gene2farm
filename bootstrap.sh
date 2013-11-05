3376d2a77b4f6e2d39bd581f4ce94a45aaf62c2d
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-11-05 18:34:53 +0100

    Add log messages and silence all unzip commmands

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,24 +9,27 @@
 log() {
     msg=$1
     date=`date`
-    printf "$date - $msg"
+    printf "$date - $msg" > /dev/stderr
 }
 
 install() {
 
   # Install missing packages
 
+  log "Install missing packages"
   sudo apt-get update --fix-missing
   sudo apt-get install -y vim cmake make g++ git unzip libncurses5-dev libncursesw5-dev wget curl python-software-properties
 
 
   # Create software folder
 
+  log "Create software folder"
   mkdir ~/soft && cd ~/soft
 
 
   # Update bash to fix the direxpand issue
 
+  log "Update bash to fix the direxpand issue"
   cd ~/soft
   git clone git://git.sv.gnu.org/bash.git
   cd bash
@@ -36,6 +39,7 @@ install() {
 
   # Install Oracle JRE
 
+  log "Install Oracle JRE"
   sudo add-apt-repository -y ppa:webupd8team/java
   sudo apt-get update
   sudo apt-get install -y oracle-java7-installer
@@ -43,6 +47,7 @@ install() {
 
   # Install BAMtools
 
+  log "Install BAMtools"
   cd ~/soft
   git clone git://github.com/pezmaster31/bamtools.git
   cd bamtools
@@ -57,6 +62,7 @@ install() {
 
   # Install BEDtools
 
+  log "Install BEDtools"
   cd ~/soft
   wget -q http://bedtools.googlecode.com/files/BEDTools.v2.17.0.tar.gz
   tar xf BEDTools.v2.17.0.tar.gz
@@ -67,6 +73,7 @@ install() {
 
   # Install Bowtie2
 
+  log "Install Bowtie2"
   cd ~/soft
   wget -q -O bowtie2-2.1.0-linux-x86_64.zip 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.1.0/bowtie2-2.1.0-linux-x86_64.zip?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fbowtie-bio%2Ffiles%2Fbowtie2%2F2.1.0%2F&ts=1375884688&use_mirror=garr'
   unzip -q bowtie2-2.1.0-linux-x86_64.zip
@@ -75,6 +82,7 @@ install() {
 
   # Install Tophat2
 
+  log "Install Tophat2"
   cd ~/soft
   wget -q http://tophat.cbcb.umd.edu/downloads/tophat-2.0.9.Linux_x86_64.tar.gz
   tar xf tophat-2.0.9.Linux_x86_64.tar.gz
@@ -83,6 +91,7 @@ install() {
 
   # Install Bwa
 
+  log "Install Bwa"
   cd ~/soft
   wget -q http://downloads.sourceforge.net/project/bio-bwa/bwa-0.7.5a.tar.bz2
   tar xf bwa-0.7.5a.tar.bz2
@@ -93,6 +102,7 @@ install() {
 
   # Install Cufflinks
 
+  log "Install Cufflinks"
   cd ~/soft
   wget -q http://cufflinks.cbcb.umd.edu/downloads/cufflinks-2.1.1.Linux_x86_64.tar.gz
   tar xf cufflinks-2.1.1.Linux_x86_64.tar.gz
@@ -101,6 +111,7 @@ install() {
 
   # Install SAMtools
 
+  log "Install SAMtools"
   cd ~/soft
   wget -q http://downloads.sourceforge.net/project/samtools/samtools/0.1.19/samtools-0.1.19.tar.bz2
   tar xf samtools-0.1.19.tar.bz2
@@ -111,6 +122,7 @@ install() {
 
   # Install Exonerate
 
+  log "Install Exonerate"
   cd ~/soft
   wget http://www.ebi.ac.uk/~guy/exonerate/exonerate-2.2.0-x86_64.tar.gz
   tar xf exonerate-2.2.0-x86_64.tar.gz
@@ -119,22 +131,25 @@ install() {
 
   # Install Fastqc
 
+  log "Install Fastqc"
   cd ~/soft
   wget -q http://www.bioinformatics.babraham.ac.uk/projects/fastqc/fastqc_v0.10.1.zip
-  unzip fastqc_v0.10.1.zip
+  unzip -q fastqc_v0.10.1.zip
   chmod 755 FastQC/fastqc
   printf '\nexport PATH=$HOME/soft/FastQC:$PATH\n' >> ~/.bashrc
 
 
   # Install GenomeAnalysisTK
 
+  log "Install GenomeAnalysisTK"
   cd ~/soft
   wget -q http://genome.crg.es/~epalumbo/gene2farm/GenomeAnalysisTK-2.7-4.tar.bz2
   tar xf GenomeAnalysisTK-2.7-4.tar.bz2
 
 
-  # Install Gmap-gsnap
+  # Install Gmap
 
+  log "Install Gmap"
   cd ~/soft
   wget -q http://research-pub.gene.com/gmap/src/gmap-gsnap-2013-10-28.tar.gz
   tar xf gmap-gsnap-2013-10-28.tar.gz
@@ -145,31 +160,35 @@ install() {
 
   # Install IGV
 
+  log "Install IGV"
   cd ~/soft
   wget -q http://genome.crg.es/~epalumbo/gene2farm/IGV_2.3.20.zip
-  unzip IGV_2.3.20.zip
+  unzip -q IGV_2.3.20.zip
   printf '\nexport PATH=$HOME/soft/IGV_2.3.20:$PATH\n' >> ~/.bashrc
 
 
   # Install Igvtools
 
+  log "Install IGVtools"
   cd ~/soft
   wget -q http://genome.crg.es/~epalumbo/gene2farm/igvtools_2.3.20.zip
-  unzip igvtools_2.3.20.zip
+  unzip -q igvtools_2.3.20.zip
   printf '\nexport PATH=$HOME/soft/IGVTools:$PATH\n' >> ~/.bashrc
 
 
   # Install Last
 
+  log "Install Last"
   cd ~/soft
   wget -q http://last.cbrc.jp/last-362.zip
-  unzip last-362.zip
+  unzip -q last-362.zip
   cd last-362
   make
   printf '\nexport PATH=$HOME/soft/last-362/src:$PATH\n' >> ~/.bashrc
 
   # Install Picard-tools
 
+  log "Install Picard-tools"
   cd ~/soft
   wget -q http://downloads.sourceforge.net/project/picard/picard-tools/1.101/picard-tools-1.101.zip
   unzip picard-tools-1.101.zip
@@ -177,6 +196,7 @@ install() {
 
   # Install Stampy
 
+  log "Install Stampy"
   cd ~/soft
   wget -q http://genome.crg.es/~epalumbo/gene2farm/stampy-1.0.22r1848.tgz
   tar xf stampy-1.0.22r1848.tgz
@@ -187,6 +207,7 @@ install() {
 
   # Install Tagdust
 
+  log "Install Tagdust"
   cd ~/soft
   wget -q http://genome.gsc.riken.jp/osc/english/software/src/tagdust.tgz
   tar xf tagdust.tgz
@@ -197,6 +218,7 @@ install() {
 
   # Install Vcftools
 
+  log "Install Vcftools"
   cd ~/soft
   wget -q http://downloads.sourceforge.net/project/vcftools/vcftools_0.1.11.tar.gz
   tar xf vcftools_0.1.11.tar.gz
@@ -207,6 +229,7 @@ install() {
 
   # Install GEM
 
+  log "Install GEMtools"
   cd ~/soft
   wget -q http://barnaserver.com/gemtools/releases/GEMTools-static-core2-1.6.2.tar.gz
   tar xf GEMTools-static-core2-1.6.2.tar.gz
@@ -215,6 +238,7 @@ install() {
 
   # Install FLUX
 
+  log "Install Flux Capacitor"
   cd ~/soft
   wget -q http://sammeth.net/artifactory/barna/barna/barna.capacitor/1.2.4/flux-capacitor-1.2.4.tgz
   tar xf flux-capacitor-1.2.4.tgz
@@ -225,9 +249,12 @@ install() {
 # Exit if already bootstrapped.
 test -f /etc/bootstrapped && exit
 
+# set log file
+logFile=$HOME/bootstrap.log
+
 export -f install
 export -f log
-su vagrant -c 'install'
+su vagrant -c "install > $logFile"
 
 # Mark as bootstrapped
 sudo sh -c "date > /etc/bootstrapped"
