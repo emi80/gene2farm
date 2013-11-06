a46a609246dca315dda6742fe7bd3223b3efbca5
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-11-06 13:51:49 +0100

    Add IGV dependencies installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -81,6 +81,7 @@ install() {
   echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
   echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
   sudo apt-get install -y oracle-java7-installer &> $javaLog
+  sudo apt-get install -y libxrender1 libxtst6 libxi6 &> $javaLog
 
   # Install BAMtools
 
