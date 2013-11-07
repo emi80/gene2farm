bb175355081b8d7a3f74930b87b437c1471301f5
Emilio Palumbo <emiliopalumbo@gmail.com>
2013-11-07 16:11:50 +0100

    Add installation of nano and nedit

--- bootstrap.sh
+++ bootstrap.sh
@@ -57,7 +57,7 @@ install() {
 
   log "Install missing packages"
   sudo apt-get update --fix-missing &> $systemLog
-  sudo apt-get install -y vim cmake make g++ git unzip gettext wget curl python-software-properties x11-apps libncurses5-dev libncursesw5-dev &> $systemLog
+  sudo apt-get install -y vim cmake make g++ git unzip gettext wget curl python-software-properties x11-apps nano nedit libncurses5-dev libncursesw5-dev &> $systemLog
 
 
   # Update bash to fix the direxpand issue
